f35d4db1846b9661abc4e474e02cc9354a090a7e
Tomasz Kalinowski <kalinowskit@gmail.com>
2019-07-01 23:31:46 -0400

    implement `break` and `next` in for loops

--- R/while.R
+++ R/while.R
@@ -1,6 +1,5 @@
 
 
-
 #' @importFrom rlang new_function
 #' @importFrom zeallot %<-%
 ag_while <- function(cond, body) {
@@ -27,42 +26,12 @@ ag_while <- function(cond, body) {
   loop_vars <- loop_vars[vapply(loop_vars, exists, TRUE, envir = env)]
 
   .cond_fn <- new_function(as_args(loop_vars), cond, env)
-
-  cond_fn <- function(did_break, loop_vars) {
+  cond_fn <- function(did_break, loop_vars)
     !did_break & do.call(.cond_fn, loop_vars)
-  }
 
-  body_w_ret <- substitute({
-    body
-    mget(loop_vars)
-  }, list(body = body, loop_vars = loop_vars))
 
-  .body_fn <- new_function(as_args(loop_vars), body_w_ret, env)
-
-  body_fn <- function(did_break, loop_vars) {
-    uncaught_loop_control_flow_registry <- Stack()
-
-    withCallingHandlers(
-      loop_vars <- do.call(.body_fn, loop_vars),
-
-      uncaught_loop_control_flow = function(lcf) {
-        uncaught_loop_control_flow_registry$push(list(
-          is_break = class(lcf)[1] == "break",
-          loop_vars = mget(names(loop_vars), lcf$env, inherits = TRUE),
-          registered_conds = reduce_registered_conds()
-        ))
-      }
-    )
-
-    while(length(uncaught_loop_control_flow_registry)) {
-      lcf <- uncaught_loop_control_flow_registry$pop()
-      c(did_break, loop_vars) %<-%
-        tf$cond(lcf$registered_conds,
-                function() list(lcf$is_break, lcf$loop_vars),
-                function() list(FALSE, loop_vars))
-    }
-    list(did_break, loop_vars)
-  }
+  .body_fn <- as_loop_fn(body, loop_vars, env)
+  body_fn <- wrap_fn_with_loop_control_flow_handlers(.body_fn)
 
   did_break <- FALSE
   loop_vars <- mget(loop_vars, envir = env, inherits = TRUE)
@@ -75,5 +44,48 @@ ag_while <- function(cond, body) {
   )
 
   list2env(res[[2]], env)
+}
+
+
+
+as_loop_fn <- function(body_expr, loop_vars, env) {
+  body_w_ret <- substitute({
+    body_expr
+    mget(loop_vars)
+  }, list(body_expr = body_expr, loop_vars = loop_vars))
+
+  new_function(as_args(loop_vars), body_w_ret, env)
+}
+
+
+wrap_fn_with_loop_control_flow_handlers <- function(body_fn) {
+  force(body_fn)
+
+  function(did_break, loop_vars) {
+    uncaught_loop_control_flow_registry <- Stack()
+
+    withCallingHandlers(
+      loop_vars <- do.call(body_fn, loop_vars),
+
+      uncaught_loop_control_flow = function(lcf) {
+        uncaught_loop_control_flow_registry$push(
+          list(
+            is_break = class(lcf)[1] == "break",
+            loop_vars = mget(names(loop_vars), lcf$env, inherits = TRUE),
+            registered_conds = reduce_registered_conds()
+          )
+        )
+      }
+    )
+
+    while (length(uncaught_loop_control_flow_registry)) {
+      lcf <- uncaught_loop_control_flow_registry$pop()
+      c(did_break, loop_vars) %<-%
+        tf$cond(lcf$registered_conds,
+                function() list(lcf$is_break, lcf$loop_vars),
+                function() list(FALSE, loop_vars))
+    }
+    list(did_break, loop_vars)
+  }
 }
 
