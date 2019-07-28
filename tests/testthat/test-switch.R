bb0df85c41651aba126c3f5d3586d20fde0a3b41
Tomasz Kalinowski <tkali@me.com>
2019-07-28 17:30:42 -0400

    implement ag_switch()

---
+++ tests/testthat/test-switch.R
@@ -0,0 +1,38 @@
+if(interactive()) {
+  devtools::load_all()
+  source("tests/testthat/utils.R")
+} else
+  source("utils.R")
+
+
+
+test_that("switch works", {
+
+  fn <- function(x) {
+    switch(x, 0, 1, 2)
+  }
+  ag_fn <- autograph(fn)
+
+  ag <- autograph
+
+  for(i in 0:2) {
+    ti <- as_tensor(i)
+    expect_equal( grab(ag(switch(ti, 0, 1, 2))), i)
+    expect_equal(grab(ag_fn(ti)), i)
+  }
+
+  ag_fn <- autograph(function(x) {
+    switch(
+      x,
+      `2` = x + 10L,
+      `0` = x,
+      `1` = x,
+      default = 99L
+    )
+  })
+
+  expect_equal( grab(ag_fn(as_tensor(2L))), 12L)
+  expect_equal( grab(ag_fn(as_tensor(0L))), 0L)
+  expect_equal( grab(ag_fn(as_tensor(-1L))), 99L)
+
+})
