5ac6c9394be35468b735d7086e9d1d603f06238b
Tomasz Kalinowski <kalinowskit@gmail.com>
2019-08-27 22:20:17 -0400

    don't enter control_dependency context in eager mode or when tracing functions

--- R/stopifnot.R
+++ R/stopifnot.R
@@ -16,9 +16,11 @@ ag_stopifnot <- function(..., exprs, local = TRUE) {
   }
 
 
-  # TODO: how to pass in custom messages?
+  # TODO: how to pass in custom messages? custom names?
   call_stack <- pretty_call_stack()
 
+  # TODO: this will fail if the symbols are objects with methods,
+  # e.g, loss_object$result()
   dots_vars <- lapply(dots, all.vars)
 
   all_vars <- mget(unique(unlist(dots_vars)), envir = env, inherits = TRUE)
@@ -44,12 +46,24 @@ ag_stopifnot <- function(..., exprs, local = TRUE) {
     list(op = op, var_nms = var_nms)
   }))
 
+  if(tf$executing_eagerly() ||
+     tf$compat$v1$get_default_graph()$building_function)
+    return(invisible())
+  # our work is done
+
+  # in tf.function contexts all ops are executed sequentially, so the mere act
+  # of creating them with tf$Assert() is enough (no need for control
+  # dependencies). If executing eagerly, similarly tf$Assert() would have raised
+  # an error upon being called.
+
+  # At this point, we're essentially only in tensorflow v1 executing normally,
+  # not in eager mode and not tracing a function.
+
   if (length(assert_ops)) {
     ops <- lapply(assert_ops, function(op) op$op)
+
     control_dependencies_context <- tf$control_dependencies(ops)
-
     control_dependencies_context$`__enter__`()
-
     vars_w_ctrl_deps <- lapply(all_vars, tf$identity)
     list2env(vars_w_ctrl_deps, envir = env)
 
