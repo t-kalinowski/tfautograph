8c8ae8923ff152cffcd7b8f50b308f158cc0dce6
Tomasz Kalinowski <kalinowskit@gmail.com>
2019-09-08 22:59:15 -0400

    implement + document minimal wrappers tf_case(), tf_switch(), tf_cond()
    Also, change tensorarray write from [<- to [[<-; import + export tensorflow::tf

--- R/methods.R
+++ R/methods.R
@@ -4,7 +4,7 @@
 # ta <- tf$TensorArray(tf$float32, size = 1L, dynamic_size = TRUE)
 
 #' @export
-`[<-.tensorflow.python.ops.tensor_array_ops.TensorArray` <-
+`[[<-.tensorflow.python.ops.tensor_array_ops.TensorArray` <-
   function(ta, i, ..., value,  name = NULL) {
     if(...length())
       stop("TensorArrays can only be written to along the first dimension (Think of them as a list())")
@@ -21,3 +21,25 @@
 # test_losses      %<>% {.$write(i, step_test_loss)}
 # train_accuracies %<>% {.$write(i, step_train_accuracy)}
 # test_accuracies  %<>% {.$write(i, step_test_accuracy)}
+
+#' @importFrom tensorflow tf_function
+#' @export
+tensorflow::tf_function
+
+
+
+
+
+as.function.formula <- function(x) {
+  envir <- environment(x)
+  body <- list(x[[length(x)]])
+  if(length(x) == 3L) {
+    args <- x[[2]]
+    if(is.call(args))
+      args[[1]] <- NULL
+  } else
+    args <- NULL
+
+  as.function.default(c(args, body), envir = envir)
+}
+
---
+++ R/tf.R
@@ -0,0 +1,211 @@
+#' tf.cond
+#'
+#' This is a minimal wrapper around `tf$cond()` that allows you to supply
+#' `true_fn` and `false_fn` as lambda functions defined using the tilde `~`.
+#'
+#' @param cond R logical or a tensor.
+#' @param true_fn,false_fn a `~` function, or something coercable to a function
+#'   via `as.function`
+#' @param name a string, passed on to `tf.cond()`
+#'
+#' @return if cond is a tensor, then the result of `tf.cond()`. Otherwise, if
+#'   `cond` is an `EagerTensor` or an R logical, then the result of either
+#'   `true_fn()` or `false_fn()`
+#' @export
+#'
+#' @examples
+#' ## square if positive
+#' # using tf$cond directly:
+#' function(x) tf$cond(x > 0, function() x * x, function() x)
+#'
+#' # using tf_cond() wrapper
+#' function(x) tf_cond(x > 0, ~ x * x, ~ x)
+tf_cond <- function(cond, true_fn, false_fn, name = NULL) {
+  env <- parent.frame()
+
+  if(is_eager_tensor(cond))
+    cond <- cond$`__bool__`()
+
+  true_fn <- as.function(true_fn)
+  false_fn <- as.function(false_fn)
+
+  if(isTRUE(cond))
+    return(true_fn())
+  if(isFALSE(cond))
+    return(false_fn())
+
+  args <- list(
+    cond = cond,
+    true_fn = true_fn,
+    false_fn = false_fn
+  )
+
+  args$name <- name
+
+  if(tf_v1())
+    args$strict <- TRUE
+
+  do.call(tf$cond, args)
+}
+
+
+#' tf.case
+#'
+#' This is a minimal wrapper around `tf.case()` that allows you to supply the
+#' `pred_fn_pairs` using the `~`.
+#'
+#' @param ...,pred_fn_pairs a list `pred_fn_pairs` supplied with the `~` like
+#'   so: `pred ~ fn_body`
+#' @param default a function, optionally specifed with the `~`, (or something
+#'   coercable to a function via `as.function()`)
+#' @param exclusive bool, whether to evaluate all `preds` and ensure only one is
+#'   true. If `FALSE` (the default), then the `preds` are evaluated in the order
+#'   supplied until the first `TRUE` value is encountered (effectively, acting
+#'   as an `if()... else if() ... else if() ...` chain)
+#' @param name a string, passed on to `tf.case()`
+#'
+#' @return The result from `tf$case()`
+#' @export
+#'
+#' @examples
+#' fizz_buzz_one <- function(x) {
+#'   tf_case(
+#'     x %% 15 == 0 ~ "FizzBuzz",
+#'     x %%  5 == 0 ~ "Buzz",
+#'     x %%  3 == 0 ~ "Fizz",
+#'     default = ~ tf$as_string(x, precision = 0L)
+#'   )
+#' }
+#'
+#' fn <- tf_function(autograph(function(n) {
+#'   for(e in tf$range(n))
+#'     tf$print(fizz_buzz_one(e))
+#' }))
+#'
+#' x <- tf$constant(16)
+#' fn(x)
+tf_case <-
+  function(...,
+           pred_fn_pairs = list(...),
+           default = NULL,
+           exclusive = FALSE,
+           name = 'case') {
+
+    env <- parent.frame()
+
+    for (i in seq_along(pred_fn_pairs)) {
+      x <- pred_fn_pairs[[i]]
+      stopifnot(inherits(x, "formula"), length(x) == 3L)
+      pred <- eval(x[[2]], env)
+      fn <- as.function(list(x[[3]]), envir = env)
+      pred_fn_pairs[[i]] <- tuple(pred, fn)
+    }
+
+    if (!is.null(default))
+      default <- as.function(default)
+
+    tf$case(
+      pred_fn_pairs,
+      default = default,
+      exclusive = exclusive,
+      strict = TRUE,
+      name = name
+    )
+  }
+
+
+
+#' tf.switch_case
+#'
+#' @param branch_index an integer tensor
+#' @param ...,branch_fns a list of function bodies specified with a `~`,
+#'   optionally supplied with a branch index on the left hand side. See examples
+#' @param default A function defined with a `~`, or something coercable via
+#'   `as.function()``
+#' @param name a string, passed on to `tf.switch_case()`
+#'
+#' @return The result from `tf.switch_case()`
+#' @export
+#'
+#' @examples
+#' tf_pow <- tf_function(function(x, pow) {
+#'    tf_switch(pow,
+#'    0 ~ 1,
+#'    1 ~ x
+#'    2 ~ x * x
+#'    3 ~ x * x * x,
+#'    default = ~ -1)
+#' })
+#'
+#' # can optionally also omit the left hand side int, in which case the order of
+#' # the functions is used.
+#' tf_pow <- function(x, pow) {
+#'   tf_switch(pow,
+#'             ~ 1,
+#'             ~ x
+#'             ~ x * x
+#'             ~ x * x * x,
+#'             default = ~ -1)
+#' }
+ #'
+#' # supply just some of the ints to override the default order
+#' tf_pow <- function(x, pow) {
+#'   tf_switch(pow,
+#'             3 ~ x * x * x,
+#'             2 ~ x * x
+#'             ~ 1,
+#'             ~ x
+#'             default = ~ -1)
+#' }
+#'
+#' # A slightly less contrived example:
+#' tf_norm <- tf_function(function(x, l) {
+#'   tf_switch(l,
+#'             0 ~ tf$reduce_sum(tf$cast(x != 0, tf$float32)), # L0 norm
+#'             1 ~ tf$reduce_sum(tf$abs(x))                    # L1 norm
+#'             2 ~ tf$sqrt(tf$reduce_sum(tf$square(x)))        # L2 norm
+#'             default = ~ tf$reduce_max(tf$abs(x)))         # L-infinity norm
+#' })
+tf_switch <- function(branch_index, ...,
+                      branch_fns = list(...),
+                      default = NULL,
+                      name = 'switch_case') {
+
+  env <- parent.frame()
+
+  if(!is.null(names(branch_fns)))
+    stop("list of branch_fns must not be named")
+
+  fns <- vector("list", length(branch_fns))
+  ints <- rep(NA_integer_, length(branch_fns))
+  for(i in seq_along(branch_fns)) {
+    fn <- branch_fns[[i]]
+    if(inherits(fn, "formula") && length(fn) == 3L) {
+      ints[[i]] <- as.integer(eval(fn[[2L]], env))
+      fn[[2L]] <- NULL
+    }
+    branch_fns[[i]] <- as.function(fn)
+  }
+
+  full_seq <- seq.int(from = 0L, along.with = branch_fns)
+  fill <- setdiff(full_seq, ints)
+  ints[is.na(ints)] <- fill
+
+  stopifnot(full_seq %in% ints)
+
+  if(!is.null(default))
+    default <- as.function(default)
+
+  tf$switch_case(
+    branch_index,
+    branch_fns = branch_fns[order(ints)],
+    default = default,
+    name = name
+  )
+}
+
+
+# maybe_call_result <- function(fn) {
+#   force(fn)
+#   function() if(is.function(res <- fn())) res() else res
+# }
