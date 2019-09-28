452d34e0f5774146b647d6115befa96df0b93b88
Tomasz Kalinowski <kalinowskit@gmail.com>
2019-09-28 15:50:28 -0400

    tf v1 compat vignette

--- R/tf-ctrl-flow-wrappers.R
+++ R/tf-ctrl-flow-wrappers.R
@@ -3,13 +3,13 @@
 #' This is a minimal wrapper around `tf$cond()` that allows you to supply
 #' `true_fn` and `false_fn` as lambda functions defined using the tilde `~`.
 #'
-#' @param cond R logical or a tensor.
+#' @param pred R logical or a tensor.
 #' @param true_fn,false_fn a `~` function, a function, or something coercible to
 #'   a function via `as.function`
 #' @param name a string, passed on to `tf.cond()`
 #'
 #' @return if cond is a tensor, then the result of `tf.cond()`. Otherwise, if
-#'   `cond` is an `EagerTensor` or an R logical, then the result of either
+#'   `pred` is an `EagerTensor` or an R logical, then the result of either
 #'   `true_fn()` or `false_fn()`
 #'
 #' @note in Tensorflow version 1, the `strict` keyword argument is supplied with
@@ -18,7 +18,7 @@
 #' @export
 #'
 #' @examples
-#' #' \dontrun{
+#' \dontrun{
 #' ## square if positive
 #' # using tf$cond directly:
 #' raw <- function(x) tf$cond(x > 0, function() x * x, function() x)
@@ -74,7 +74,7 @@ tf_cond <- function(pred, true_fn, false_fn, name = NULL) {
 #' @export
 #'
 #' @examples
-#' #' \dontrun{
+#' \dontrun{
 #' fizz_buzz_one <- function(x) {
 #'   tf_case(
 #'     x %% 15 == 0 ~ "FizzBuzz",
@@ -139,7 +139,7 @@ tf_case <-
 #' @export
 #'
 #' @examples
-#' #' \dontrun{
+#' \dontrun{
 #' tf_pow <- tf_function(function(x, pow) {
 #'    tf_switch(pow,
 #'    0 ~ 1,
