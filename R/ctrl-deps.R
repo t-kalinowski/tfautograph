24da870debe0cd1b3df5f6b8fca6e97ecbde3b9f
Tomasz Kalinowski <kalinowskit@gmail.com>
2019-10-01 11:13:58 -0400

    move rlang from Imports to Suggests

--- R/ctrl-deps.R
+++ R/ctrl-deps.R
@@ -3,10 +3,15 @@
 
 
 ag_on.exit <- function(expr = NULL, add = TRUE, after = FALSE) {
+
+  if(!add) {
+    stop
+  }
   env <- parent.frame()
-  if(!add && !is.null(rlang::eval_bare(quote(sys.on.exit()), env)))
-    stop("autograph() requires that `add = TRUE` be specified in any `on.exit` calls.
-    This is because be autographing functions that register control dependencies
+  if(!add &&
+     !requireNamespace("rlang") || !is.null(rlang::eval_bare(quote(sys.on.exit()), env)))
+    stop("autograph() requires that `add = TRUE` be specified in this `on.exit` call.
+    This is because autographing functions that register control dependencies
     like `stopifnot()` use the `on.exit()` mechanism to ensure those control dependencies
     are properly captured. If `add = FALSE` then those on.exit hooks would otherwise be overwritten")
   cl <- as.call(c(quote(base::on.exit), substitute(expr),
@@ -16,6 +21,8 @@ ag_on.exit <- function(expr = NULL, add = TRUE, after = FALSE) {
 
 
 on.exit.elsewhere <- function(expr = NULL, add = TRUE, after = TRUE, envir = parent.frame(2)) {
+  if(!requireNamespace("rlang"))
+    stop("Package `rlang` is required for tfautograph to capture control dependencies if you are in graph mode and outside a tf.function() context.")
   cl <- as.call(c(quote(base::on.exit), substitute(expr),
                   add = add, after = after))
   rlang::eval_bare(cl, envir)
